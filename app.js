c746a5e6291d2ff9f530bfe2d89ee0c6630d5c24
40183396 <40183396@live.napier.ac.uk>
2018-08-14 19:31:47 +0100

    Fixed error with replying to comments and commenting on stream

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const tmi = require('tmi.js');
 const apikey = "oauth:ympmbfqrgzl3yi3q96kkt4s9ly5ybq"
 const name = "my_chat_bot_"
+const channel1 = "my_chat_bot_"
 
-const options = {
+let options = {
     options: {
         debug: true
     },
@@ -14,13 +15,19 @@ const options = {
         username: name,
         password: apikey
     },
-    channels: ["room1"]
+    channels: [channel1]
 }
 
 // connect to IRC
-const client = new tmi.client(options)
+let client = new tmi.client(options)
 client.connect();
+
+client.on("chat", function (channel, userstate, message, self) {
+    client.action(channel1, userstate['display-name'] + " you are a total noob")
+});
 
 client.on('connected', (address, port) => {
     console.log("Address: " + address + " " + "Port: " + port);
+    client.action(channel1, "Hi there bro");
 })
+
