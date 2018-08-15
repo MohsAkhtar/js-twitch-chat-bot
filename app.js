542b9ec47f9df79f9bb239403715a48b2fa5bc64
40183396 <40183396@live.napier.ac.uk>
2018-08-15 12:34:31 +0100

    Cleaned up code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const tmi = require('tmi.js');
 const apikey = "oauth:ympmbfqrgzl3yi3q96kkt4s9ly5ybq"
-const name = "my_chat_bot_"
-const channel1 = "my_chat_bot_"
+const botName = "my_chat_bot_"
+// Channel bot connects to
+const connectedChannel = "my_chat_bot_"
 
-let options = {
+const options = {
     options: {
         debug: true
     },
@@ -12,22 +13,24 @@ let options = {
         reconnect: true
     },
     identity: {
-        username: name,
+        username: botName,
         password: apikey
     },
-    channels: [channel1]
+    channels: [connectedChannel]
 }
 
 // connect to IRC
 let client = new tmi.client(options)
 client.connect();
 
+// On when a user enters a message in chat, bot replies to that user
 client.on("chat", function (channel, userstate, message, self) {
-    client.action(channel1, userstate['display-name'] + " you are a total noob")
+    client.action(connectedChannel, userstate['display-name'] + " you are a total noob")
 });
 
+// On bots connection
 client.on('connected', (address, port) => {
     console.log("Address: " + address + " " + "Port: " + port);
-    client.action(channel1, "Hi there bro");
+    client.action(connectedChannel, "Hi there bro");
 })
 
